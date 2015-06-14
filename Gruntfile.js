d7baae2099916d5b5621d5e134129ebe5980900e
Remco Tolsma <info@remcotolsma.nl>
2015-06-14 14:04:39 +0200

    Added more conversions.

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,12 +33,19 @@ module.exports = function( grunt ) {
 
 			inkscape: {
 				command: [
+					// PNG
 					'inkscape --export-png=converted/inkscape/png/ideal-square-20pct-margin/ideal-square-20pct-margin-512x12.png --export-width=512 --export-height=512 src/ideal-square-20pct-margin/ideal-square-20pct-margin.svg',
 					'inkscape --export-png=converted/inkscape/png/ideal-square-20pct-margin/ideal-square-20pct-margin-256x256.png --export-width=256 --export-height=256 src/ideal-square-20pct-margin/ideal-square-20pct-margin.svg',
 					'inkscape --export-png=converted/inkscape/png/ideal-square-20pct-margin/ideal-square-20pct-margin-128x128.png --export-width=128 --export-height=128 src/ideal-square-20pct-margin/ideal-square-20pct-margin.svg',
 					'inkscape --export-png=converted/inkscape/png/ideal-square-20pct-margin/ideal-square-20pct-margin-64x64.png --export-width=64 --export-height=64 src/ideal-square-20pct-margin/ideal-square-20pct-margin.svg',
 					'inkscape --export-png=converted/inkscape/png/ideal-square-20pct-margin/ideal-square-20pct-margin-32x32.png --export-width=32 --export-height=32 src/ideal-square-20pct-margin/ideal-square-20pct-margin.svg',
 					'inkscape --export-png=converted/inkscape/png/ideal-square-20pct-margin/ideal-square-20pct-margin-16x16.png --export-width=16 --export-height=16 src/ideal-square-20pct-margin/ideal-square-20pct-margin.svg',
+					// SVG
+					'inkscape --export-plain-svg=converted/inkscape/svg/ideal-square-20pct-margin/ideal-square-20pct-margin.svg src/ideal-square-20pct-margin/ideal-square-20pct-margin.svg',
+					// EPS
+					'inkscape --export-eps=converted/inkscape/eps/ideal-square-20pct-margin/ideal-square-20pct-margin.eps src/ideal-square-20pct-margin/ideal-square-20pct-margin.svg',
+					// PDF
+					'inkscape --export-pdf=converted/inkscape/pdf/ideal-square-20pct-margin/ideal-square-20pct-margin.pdf src/ideal-square-20pct-margin/ideal-square-20pct-margin.svg'
 				].join( '&&' )
 			},
 
