6d3eb5e9f1877705018036b5cad02b69a0151250
Remco Tolsma <info@remcotolsma.nl>
2015-06-14 14:34:21 +0200

    Added square no margin variants.

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,7 +45,21 @@ module.exports = function( grunt ) {
 					// EPS
 					'inkscape --export-eps=converted/inkscape/eps/ideal-square-20pct-margin/ideal-square-20pct-margin.eps src/ideal-square-20pct-margin/ideal-square-20pct-margin.svg',
 					// PDF
-					'inkscape --export-pdf=converted/inkscape/pdf/ideal-square-20pct-margin/ideal-square-20pct-margin.pdf src/ideal-square-20pct-margin/ideal-square-20pct-margin.svg'
+					'inkscape --export-pdf=converted/inkscape/pdf/ideal-square-20pct-margin/ideal-square-20pct-margin.pdf src/ideal-square-20pct-margin/ideal-square-20pct-margin.svg',
+
+					// PNG
+					'inkscape --export-png=converted/inkscape/png/ideal-square-no-margin/ideal-square-no-margin-512x12.png --export-width=512 --export-height=512 src/ideal-square-no-margin/ideal-square-no-margin.svg',
+					'inkscape --export-png=converted/inkscape/png/ideal-square-no-margin/ideal-square-no-margin-256x256.png --export-width=256 --export-height=256 src/ideal-square-no-margin/ideal-square-no-margin.svg',
+					'inkscape --export-png=converted/inkscape/png/ideal-square-no-margin/ideal-square-no-margin-128x128.png --export-width=128 --export-height=128 src/ideal-square-no-margin/ideal-square-no-margin.svg',
+					'inkscape --export-png=converted/inkscape/png/ideal-square-no-margin/ideal-square-no-margin-64x64.png --export-width=64 --export-height=64 src/ideal-square-no-margin/ideal-square-no-margin.svg',
+					'inkscape --export-png=converted/inkscape/png/ideal-square-no-margin/ideal-square-no-margin-32x32.png --export-width=32 --export-height=32 src/ideal-square-no-margin/ideal-square-no-margin.svg',
+					'inkscape --export-png=converted/inkscape/png/ideal-square-no-margin/ideal-square-no-margin-16x16.png --export-width=16 --export-height=16 src/ideal-square-no-margin/ideal-square-no-margin.svg',
+					// SVG
+					'inkscape --export-plain-svg=converted/inkscape/svg/ideal-square-no-margin/ideal-square-no-margin.svg src/ideal-square-no-margin/ideal-square-no-margin.svg',
+					// EPS
+					'inkscape --export-eps=converted/inkscape/eps/ideal-square-no-margin/ideal-square-no-margin.eps src/ideal-square-no-margin/ideal-square-no-margin.svg',
+					// PDF
+					'inkscape --export-pdf=converted/inkscape/pdf/ideal-square-no-margin/ideal-square-no-margin.pdf src/ideal-square-no-margin/ideal-square-no-margin.svg'
 				].join( '&&' )
 			},
 
@@ -57,6 +71,8 @@ module.exports = function( grunt ) {
 					'convert -background transparent -resize 64x64 src/ideal-square-20pct-margin/ideal-square-20pct-margin.svg converted/imagemagick/png/ideal-square-20pct-margin/ideal-square-20pct-margin-64x64.png',
 					'convert -background transparent -resize 32x32 src/ideal-square-20pct-margin/ideal-square-20pct-margin.svg converted/imagemagick/png/ideal-square-20pct-margin/ideal-square-20pct-margin-32x32.png',
 					'convert -background transparent -resize 16x16 src/ideal-square-20pct-margin/ideal-square-20pct-margin.svg converted/imagemagick/png/ideal-square-20pct-margin/ideal-square-20pct-margin-16x16.png',
+
+					'convert -background transparent -resize 16x16 src/ideal-square-no-margin/ideal-square-no-margin.svg converted/imagemagick/png/ideal-square-no-margin/ideal-square-no-margin-16x16.png',
 				].join( '&&' )
 			}
 		},
