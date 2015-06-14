8d438c3a1f900ed121d8eb8cbe22131a1e006b4a
Remco Tolsma <info@remcotolsma.nl>
2015-06-14 13:42:43 +0200

    Working on automation.

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,11 +25,35 @@ module.exports = function( grunt ) {
 
 		// Shell
 		shell: {
-			dirListing: {
+			svgexport: {
 				command: [
-					'svgexport src/ideal/ideal.svg svgexport/ideal-128.png 128:',
-					'svgexport src/ideal/ideal.svg svgexport/ideal-512.png 512:512'
+					'svgexport svgexport.json'
 				].join( '&&' )
+			},
+
+			inkscape: {
+				command: [
+					'inkscape --export-png=converted/inkscape/png/ideal-square-20pct-margin/ideal-square-20pct-margin-256x256.png --export-width=256 --export-height=256 src/ideal-square-20pct-margin/ideal-square-20pct-margin.svg',
+					'inkscape --export-png=converted/inkscape/png/ideal-square-20pct-margin/ideal-square-20pct-margin-128x128.png --export-width=128 --export-height=128 src/ideal-square-20pct-margin/ideal-square-20pct-margin.svg',
+				].join( '&&' )
+			},
+
+			imagemagick: {
+				command: [
+					'convert -resize 256x256 src/ideal-square-20pct-margin/ideal-square-20pct-margin.svg converted/imagemagick/png/ideal-square-20pct-margin/ideal-square-20pct-margin-256x256.png'
+				].join( '&&' )
+			}
+		},
+
+		// Image Minify
+		imagemin: {
+			dynamic: {
+				files: [ {
+					expand: true,
+					cwd: 'converted/',
+					src: ['**/*.{png,jpg,gif}'],
+					dest: 'minified/'
+				} ]
 			}
 		},
 
@@ -44,11 +68,12 @@ module.exports = function( grunt ) {
 	} );
 
 	// Load
+	grunt.loadNpmTasks( 'grunt-contrib-imagemin' );
 	grunt.loadNpmTasks( 'grunt-imagemagick' );
 	grunt.loadNpmTasks( 'grunt-rasterize' );
 	grunt.loadNpmTasks( 'grunt-shell' );
 	grunt.loadNpmTasks( 'grunt-svg-modify' );
 
 	// Tasks
-	grunt.registerTask( 'default', [ 'imagemagick-convert' ] );
+	grunt.registerTask( 'default', [ 'shell', 'imagemin' ] );
 };
